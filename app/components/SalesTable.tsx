e36820720c83c2c5575d1d9be67664deb9dbabfd
Brian Hyun <brianhyun@protonmail.com>
2025-01-17 21:41:10 -0800

    Remove TableDataCell implementation in shared Table component

--- app/components/SalesTable.tsx
+++ app/components/SalesTable.tsx
@@ -1,20 +1,41 @@
 import { useSelector } from "react-redux";
 import { RootState } from "@/lib/store";
 import { Table } from "./Table";
+import { Container } from "./Container";
+import { TableDataCell } from "./TableDataCell";
+import { formatDate } from "@/utils/date";
+import { WeeklySalesData } from "@/types/sales";
 
 export function SalesTable() {
   const { sales } = useSelector((state: RootState) => state.sales);
 
   return (
-    <Table
-      data={sales}
-      headers={[
-        "Week Ending",
-        "Retail Sales",
-        "Wholesale Sales",
-        "Units Sold",
-        "Retailer Margin",
-      ]}
-    />
+    <Container className="p-6 h-full">
+      <Table
+        headers={[
+          "Week Ending",
+          "Retail Sales",
+          "Wholesale Sales",
+          "Units Sold",
+          "Retailer Margin",
+        ]}
+      >
+        {sales.map((sale: WeeklySalesData) => (
+          <tr key={sale.weekEnding}>
+            <TableDataCell value={formatDate(sale.weekEnding)} />
+            <TableDataCell
+              value={`$${new Intl.NumberFormat().format(sale.retailSales)}`}
+            />
+            <TableDataCell
+              value={`$${new Intl.NumberFormat().format(sale.wholesaleSales)}`}
+            />
+            <TableDataCell value={sale.unitsSold} />
+            <TableDataCell
+              value={`$${new Intl.NumberFormat().format(sale.retailerMargin)}`}
+            />
+          </tr>
+        ))}
+      </Table>
+    </Container>
   );
 }
--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,56 +1,30 @@
-import { WeeklySalesData } from "@/types/sales";
-import { Container } from "./Container";
 import { TableHeader } from "./TableHeader";
-import { TableDataCell } from "./TableDataCell";
-import { formatDate } from "@/utils/date";
 
 export function Table({
-  data,
+  children,
   headers,
 }: {
-  data: WeeklySalesData[];
+  children: React.ReactNode;
   headers: string[];
 }) {
   return (
-    <Container className="p-6 h-full">
-      <div className="flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full align-middle sm:px-6 lg:px-8">
-            <table className="min-w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  {headers.map((header) => (
-                    <TableHeader title={header} key={header} />
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 bg-white">
-                {data.map((sale) => (
-                  <tr key={sale.weekEnding}>
-                    <TableDataCell value={formatDate(sale.weekEnding)} />
-                    <TableDataCell
-                      value={`$${new Intl.NumberFormat().format(
-                        sale.retailSales
-                      )}`}
-                    />
-                    <TableDataCell
-                      value={`$${new Intl.NumberFormat().format(
-                        sale.wholesaleSales
-                      )}`}
-                    />
-                    <TableDataCell value={sale.unitsSold} />
-                    <TableDataCell
-                      value={`$${new Intl.NumberFormat().format(
-                        sale.retailerMargin
-                      )}`}
-                    />
-                  </tr>
+    <div className="flow-root">
+      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full align-middle sm:px-6 lg:px-8">
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                {headers.map((header) => (
+                  <TableHeader title={header} key={header} />
                 ))}
-              </tbody>
-            </table>
-          </div>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {children}
+            </tbody>
+          </table>
         </div>
       </div>
-    </Container>
+    </div>
   );
 }
